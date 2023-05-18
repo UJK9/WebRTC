8eb0d2e85f8d62c7069626c11cd5c070ccf3e4b7
Ujjwal Jung Karki <ujjwaljk9@gmail.com>
2023-05-18 23:03:42 +0545

    Second edit

--- receive/receive.js
+++ receive/receive.js
@@ -1,34 +1,37 @@
 const webSocket = new WebSocket("ws://127.0.0.1:3000");
 
 webSocket.onmessage = (event) => {
-    handleSignallingData(JSON.parse(event.data))
-}
+    handleSignallingData(JSON.parse(event.data));
+};
 
-function handleSignallingData(data){
-    switch(data.type){
+function handleSignallingData(data) {
+    switch (data.type) {
         case "offer":
             peerConnection.setRemoteDescription(new RTCSessionDescription(data.offer))
-            .then(() => {
-                createAndSendAnswer();
-            })
+                .then(() => {
+                    createAndSendAnswer();
+                });
             break;
         case "candidate":
-            peerConnection.addIceCandidate(new RTCIceCandidate(data.candidate))
+            peerConnection.addIceCandidate(new RTCIceCandidate(data.candidate));
             break;
     }
 }
 
-function createAndSendAnswer(){
-    peerConnection.createAnswer().then((answer) => {
-        return peerConnection.setLocalDescription(answer);
-    }).then(() => {
-        sendData({
-            type: "send_answer",
-            answer: peerConnection.localDescription.toJSON()
+function createAndSendAnswer() {
+    peerConnection.createAnswer()
+        .then((answer) => {
+            return peerConnection.setLocalDescription(answer);
+        })
+        .then(() => {
+            sendData({
+                type: "send_answer",
+                answer: peerConnection.localDescription.toJSON()
+            });
+        })
+        .catch(error => {
+            console.log(error);
         });
-    }).catch(error => {
-        console.log(error);
-    });
 }
 
 function sendData(data) {
@@ -45,6 +48,27 @@ function joinCall() {
     document.getElementById("video-call-div").style.display = "inline";
     navigator.mediaDevices.getUserMedia({
         video: {
-            frameRate: 36, 
+            frameRate: 36,
             width: {
                 min: 480
+            }
+        }
+    })
+        .then(stream => {
+            localStream = stream;
+            document.getElementById("local-video").srcObject = stream;
+
+            const configuration = {
+                iceServers: [
+                    { urls: "stun:stun.l.google.com:19302" },
+                    { urls: "turn:turn", username: "user", credential: "password" }
+                ]
+            };
+
+            peerConnection = new RTCPeerConnection(configuration);
+            peerConnection.addStream(localStream);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
